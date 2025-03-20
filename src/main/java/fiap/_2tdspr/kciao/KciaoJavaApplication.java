27d92eb918f3c6bf2d69fb40e694868c6926bd7f
VivianSTWu <vivian.stwu@gmail.com>
2025-03-19 23:07:47 -0300

    alteracao para deploy web app

--- src/main/java/fiap/_2tdspr/kciao/KciaoJavaApplication.java
+++ src/main/java/fiap/_2tdspr/kciao/KciaoJavaApplication.java
@@ -2,12 +2,18 @@ package fiap._2tdspr.kciao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KciaoJavaApplication {
+public class KciaoJavaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KciaoJavaApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(KciaoJavaApplication.class);
+	}
 }
