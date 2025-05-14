ad04e86cbf3e29f9d1eeaba65883af24faeaf098
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2025-05-14 20:02:36 -0300

    add form fill route

--- src/main/java/fiap/_2tdspr/kciao/configs/SecurityConfig.java
+++ src/main/java/fiap/_2tdspr/kciao/configs/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
 //                        .anyRequest().permitAll()
+                        //auth
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
